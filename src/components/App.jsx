0cf89fa4eab2769f615758916742ea95212eeb47
Mark Shliakhov <valmarksh@gmail.com>
2022-12-29 18:28:59 +0200

    refactoring to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,45 @@
-import { React, Component } from 'react';
+import { React, useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 import css from './Section/Section.module.css';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
 
-  onclickIncrement = option => {
-    this.setState(prevState => {
-      return { [option]: prevState[option] + 1 };
-    });
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const onclickIncrement = option => {
+    if (option === 'good') setGood(prev => prev + 1);
+    if (option === 'neutral') setNeutral(prev => prev + 1);
+    if (option === 'bad') setBad(prev => prev + 1);
+  };
+  const countTotalFeedback = () => {
     const totalFeedback = good + neutral + bad;
     return totalFeedback.toString();
   };
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
 
+  const countPositiveFeedbackPercentage = () => {
     const positivePercent = Math.floor((good / (good + neutral + bad)) * 100);
     return positivePercent;
   };
 
-  showStatistics = () => {
-    return this.state.good || this.state.neutral || this.state.bad;
-  };
+  const buttons = Object.keys({ good, neutral, bad });
+  return (
+    <section className={css.section}>
+      <FeedbackOptions buttons={buttons} onclickIncrement={onclickIncrement} />
 
-  render() {
-    const buttons = Object.keys(this.state);
-    const total = this.countTotalFeedback();
-    const posPerc = this.countPositiveFeedbackPercentage();
-
-    return (
-      <section className={css.section}>
-        <FeedbackOptions
-          buttons={buttons}
-          onclickIncrement={this.onclickIncrement}
+      {good || neutral || bad ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
         />
-
-        {this.state.good || this.state.neutral || this.state.bad ? (
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={total}
-            positivePercentage={posPerc}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </section>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </section>
+  );
+};
